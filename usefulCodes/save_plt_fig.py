ccec77eeb4f7536c580465871b788f5a34bbb48f
xianpf <46647600+xianpf@users.noreply.github.com>
2022-01-21 21:26:48 +0800

    save plt figures : use histogram as example
    save plt figures : use histogram as example

---
+++ usefulCodes/save_plt_fig.py
@@ -0,0 +1,11 @@
+
+    def prob_histgram(self, pixel_probs_k):
+        from io import BytesIO
+        from PIL import Image
+        buffer = BytesIO()
+        _ = plt.hist(pixel_probs_k.cpu().detach().view(-1).numpy(), 100)
+        plt.savefig(buffer, format='png')
+        plt.clf()
+        buffer.seek(0)
+        im = np.array(Image.open(buffer))
+        return im
